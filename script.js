84f0a0153c21b49db63af967475456540a2e827a
Dome1992 <forlanodomenico92@mail.com>
2023-12-01 16:55:59 +0100

    inserite foto

---
+++ script.js
@@ -0,0 +1,9 @@
+//Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+//MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+
+//MILESTONE 1: Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+//MILESTONE 2: Stampare le stesse informazioni su DOM sotto forma di stringhe
+
+
